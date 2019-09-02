083b81c511543f4d74d63f169179dcc836f3cba0
Caoimhe Malone <caoimhemalone@Caoimhes-MacBook-Air.local>
2019-09-02 22:22:44 +0100

    gallry updated

--- Bodynamics_final/gallery.php
+++ Bodynamics_final/gallery.php
@@ -1,4 +1,118 @@
-<?php include("header.php");?>
+<!-- Author: Caoimhe Malone (key-vah) -->
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-W5G6FZG');</script>
+    <!-- End Google Tag Manager -->
+    
+    <meta charset="utf-8">
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Photos of Bodynamics gym">
+    <meta name="title" content="Bodynamics">
+    <meta name="author" content="Caoimhe Malone">
+    <meta name="google-site-verification" content="google-site-verification=bS8o4lx8sXOJVespTrHslz-xvH03vi-8_XrvLehWv9M">
+
+    <title>Bodynamics photo gallery</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
+    <link rel="canonical" href="https://bodynamics.ie/gallery" />
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="styles/main.css">
+    <!-- Bootstrap CSS -->
+    <link href="Bootstrap/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow" rel="stylesheet">
+    
+    <!-- For fade in -->
+     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+     <script type="module" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.esm.js"></script>
+     <script nomodule="" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.js"></script>
+     
+     <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139553932-1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-139553932-1');
+      </script>
+
+    
+  </head>
+
+
+    <body>
+
+      <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W5G6FZG"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <!-- End Google Tag Manager (noscript) -->
+
+    <div id="home"></div>
+
+    <!-- Nav Bar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" alt="nav">
+      <div class="container ">
+        <a class="navbar-brand" href="/">
+            <img alt="logo image"src="images/logo.png" alt="Bodynamics"/>
+        </a>
+            <button id="nav-toggle" class="navbar-toggler hamburgerBtn" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <a href="#toggle" class="hamburgler">
+              <div class="bun top"></div>
+              <div class="meat"></div>
+              <div class="bun bottom"></div>
+          </a>
+              <!-- <span class="navbar-toggler-icon"></span> -->
+            </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" id="homeNav" >Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="membershipsNav">Pricing</a> 
+            </li>
+            <li class="nav-item">
+              <a  class="nav-link" id="teamNav">Team</a>
+            </li>
+            <li class="nav-item">
+              <a  class="nav-link" id="timetableNav">Timetable</a>
+            </li>
+            <li class="nav-item">
+              <a  class="nav-link" id="aboutNav">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="galleryNav" href="/gallery">Gallery</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="blogNav" href="/blog">Blog</a>
+            </li>
+            
+          </ul>
+          <div class="signUpNav">
+            <a  class="signUpBtn" target="_blank" href="https://bodynamics.wodify.com/OnlineSalesPortal/Home.aspx"> Sign Up </a>
+          </div>
+        </div>
+      </div>
+
+    </nav>
+
 
 <style type="text/css">
     section {
@@ -45,9 +159,104 @@
             width: 30%;
         }
     }
+    
+    .gallery h2 {
+        margin-top: 2.5em;
+    }
 </style>
 <section class="gallery">
     <div class="container">
+        <h2> Summer Games 2019</h2>
+        <!--Start of summer games-->
+        <div class="project-media row">
+            <div class="col-md-6">
+                <img data-src="gallery/IMG_0335.jpg" alt="Bodynamics Gym">
+                <img data-src="gallery/IMG_0600.jpg" alt="Bodynamics Gym">
+            </div>
+            <div class="col-md-6">
+                <img data-src="gallery/IMG_0009.jpg" alt="Bodynamics Gym">
+                <img data-src="gallery/IMG_0200.jpg" alt="Bodynamics Gym">
+            </div>
+        </div>
+        <div class="project-media row">
+            <div class="col-md-4">
+                <img data-src="gallery/IMG_0499.jpg" alt="Bodynamics Gym">
+            </div>   
+        
+            <div class="col-md-4">
+                <img data-src="gallery/IMG_0574.jpg" alt="Bodynamics Gym">
+            </div>
+             <div class="col-md-4">
+                <img data-src="gallery/IMG_0790.jpg" alt="Bodynamics Gym">
+            </div>
+        </div>
+        <div class="project-media row">
+             <div class="col-md-6">
+                <img data-src="gallery/IMG_0734.jpg" alt="Bodynamics Gym">
+            </div>
+            <div class="col-md-6">
+                <img data-src="gallery/IMG_0536.jpg" alt="Bodynamics Gym">
+                <img data-src="gallery/IMG_0025.jpg" alt="Bodynamics Gym">
+            </div>
+        </div>
+         
+         <div class="project-media row">
+            <div class="col-md-4">
+                <img data-src="gallery/IMG_1473.jpg" alt="Bodynamics Gym">
+            </div>   
+        
+            <div class="col-md-4">
+                <img data-src="gallery/IMG_1193.jpg" alt="Bodynamics Gym">
+            </div>
+             <div class="col-md-4">
+                <img data-src="gallery/IMG_1285.jpg" alt="Bodynamics Gym">
+            </div>
+        </div> 
+        <div class="project-media row">
+            <div class="col-md-6">
+                <img data-src="gallery/IMG_1517.jpg" alt="Bodynamics Gym">
+            </div>   
+        
+            <div class="col-md-6">
+                <img data-src="gallery/IMG_1264.jpg" alt="Bodynamics Gym">
+            </div>
+
+        </div> 
+        <div class="project-media row">
+            <div class="col-md-4">
+                <img data-src="gallery/IMG_1584.jpg" alt="Bodynamics Gym">
+            </div>   
+        
+            <div class="col-md-4">
+                <img data-src="gallery/IMG_1689.jpg" alt="Bodynamics Gym">
+            </div>
+             <div class="col-md-4">
+                <img data-src="gallery/IMG_1957.jpg" alt="Bodynamics Gym">
+            </div>
+        </div>
+         <div class="project-media row">
+            <div class="col-md-6">
+                <img data-src="gallery/IMG_2072.jpg" alt="Bodynamics Gym">
+            </div>   
+        
+            <div class="col-md-6">
+                <img data-src="gallery/IMG_2093.jpg" alt="Bodynamics Gym">
+            </div>
+
+        </div>
+        <div class="project-media row">
+            <div class="col-md-6">
+                <img data-src="gallery/IMG_2107.jpg" alt="Bodynamics Gym">
+            </div>   
+        
+            <div class="col-md-6">
+                <img data-src="gallery/IMG_2121.jpg" alt="Bodynamics Gym">
+            </div>
+
+        </div>
+        <!-- End of summer games-->
+        
+        <h2>Gallery</h2>
         <div class="project-media row">
             <div class="col-md-4">
                 <img data-src="gallery/bd_10.jpg" alt="Bodynamics Gym">
